ecdbdab86832eb131eb8ed34e8a66f4dfdb0691a
Vu Phan <ngocvuphan@gmail.com>
2024-12-29 17:46:31 +0700

    v0.1.2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8ff724b753f377ee487be4599e0a66a4",
+const RESOURCES = {"flutter_bootstrap.js": "1bec45259d294fc5137d9fecef4aa3aa",
 "version.json": "ff966ab969ba381b900e61629bfb9789",
 "index.html": "e638b8e65c23b3c24b881093cb9c8718",
 "/": "e638b8e65c23b3c24b881093cb9c8718",
-"main.dart.js": "c884b4608ae9d482ec74e84707103f0d",
+"main.dart.js": "56d303fd4b1f63a7857e99e34b6b7b00",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
