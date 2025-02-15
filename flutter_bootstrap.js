bc18204b6a33a2392cd892bef58346cb72bbb917
Vu Phan <ngocvuphan@gmail.com>
2025-02-15 15:47:28 +0700

    v0.1.3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "839573337"
+    serviceWorkerVersion: "2838660702"
   }
 });
